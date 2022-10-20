96a10783cc96f663232bc83721df7aa5edc75b6c
GentleMizt <olamideiyanda18@gmail.com>
2022-10-20 17:03:19 +0100

    5th commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let body = document.body;
 let input = document.getElementById('amountSpent');
 let select = document.getElementById('rating');
+let tip = document.getElementById('tip')
 let btn = document.getElementById('calculate');
+
+btn.addEventListener('click', callBackFunction)
+
+function callBackFunction(){
+    // User must specify value and rating
+    if (input.value.length === '' || select.value === ''){
+        alert('Boss, you need to put in a amount as well as a rating');
+        return;
+    }
+    
+    const myTip = input.value * select.value;
+    console.log(myTip);
+}
